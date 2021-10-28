f20be92d9043ecb915e06cd14f2c91857f021f84
cloudsecuritylabs <basu.ankan@gmail.com>
2021-10-28 11:05:54 -0400

    Updated RGs to include tags

--- terraform/rg/main.tf
+++ terraform/rg/main.tf
@@ -7,5 +7,7 @@ provider "azurerm" {
 resource "azurerm_resource_group" "name" {
     name = "resouceGroupByTerraform"
     location = "eastus"
+    # update code with tags
+    tags = {"lifecycle": "prod", "author" : "terraform"}
   
 }
